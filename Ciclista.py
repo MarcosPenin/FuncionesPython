84ee5d5fdaf40d9ed2fb56cc65456098e135b799
Marcos González Penín <a20marcosgp@ateis.local>
2021-09-30 20:00:30 +0200

    Corregido para tener en cuenta los límites horarios git push

--- Ciclista.py
+++ Ciclista.py
@@ -9,7 +9,15 @@ segundosSalida=int(input("Segundo:"))
 T=int(input("¿Cuántos segundos tardará en llegar a la ciudad B?"))
 
 horaLlegada = horaSalida + T//3600
+if(horaLlegada>23):
+    horaLlegada-=24
 minutosLlegada = minutosSalida + (T%3600)//60 
+if(minutosLlegada>59):
+    minutosLlegada-=60
+    horaLlegada+=1
 segundosLlegada = segundosSalida + (T%3600)%60
+if(segundosLlegada>60):
+    segundosLlegada-=60
+    minutosLlegada+=1
 
 print("El ciclista llegará a las",horaLlegada, "horas", minutosLlegada, "minutos", segundosLlegada,"segundos")
