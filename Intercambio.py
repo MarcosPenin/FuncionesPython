a54e0b313046ee4b664e86fd4705e5d221233bbd
Marcos González Penín <a20marcosgp@ATEIS.LOCAL>
2021-09-30 19:20:59 +0200

    En Python se pueden intercambiar dos variables a la vez

--- Intercambio.py
+++ Intercambio.py
@@ -3,8 +3,7 @@ print("Intercambio de valor de variables")
 a=float(input("Introduce la variable A"))
 b=float(input("Introduce la variable B"))
 
-c=a
-a=b
-b=c
+a,b=b,a
 
 print("¡Intercambio! Ahora A vale",a,"y B vale",b)
+
