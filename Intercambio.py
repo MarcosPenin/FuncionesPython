7c00307eb058ad885558d3fbebdc3feee6e4d739
Marcos González Penín <a20marcosgp@ATEIS.LOCAL>
2021-09-30 19:17:09 +0200

    Intercambio

---
+++ Intercambio.py
@@ -0,0 +1,10 @@
+print("Intercambio de valor de variables")
+
+a=float(input("Introduce la variable A"))
+b=float(input("Introduce la variable B"))
+
+c=a
+a=b
+b=c
+
+print("¡Intercambio! Ahora A vale",a,"y B vale",b)
