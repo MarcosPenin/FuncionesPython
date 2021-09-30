f9e648c59bdfc2a965a434253a03ad6daaf1417b
Marcos González Penín <a20marcosgp@ATEIS.LOCAL>
2021-09-30 18:48:26 +0200

    Primera práctica

---
+++ Primero.py
@@ -0,0 +1,20 @@
+
+import math
+
+print("CÁLCULO DE LA HIPOTENUSA DE UN TRIÁNGULO RECTÁNGULO")
+
+cateto1=float(input("Introduce el valor del primer cateto:"))
+cateto2=float(input("Introduce el valor del segundo cateto"))
+
+hipotenusa=math.sqrt(pow(cateto1,2)+pow(cateto2,2))
+
+print("La hipotenusa es",hipotenusa)
+
+
+
+
+
+
+
+
+#La hipotenusa de un triángulo rectángulo es igual a la raiz cuadrada de la suma del cuadrado de los catet 
